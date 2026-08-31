0b3764c97d264a1bb5472230ac70fc4117c690e1
deepikapoojari388-rgb <deepikapoojari388@gmail.com>
2026-08-31 13:10:31 +0530

    Add files via upload

---
+++ Blog.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            trim: true
+        },
+
+        category: {
+            type: String,
+            trim: true
+        },
+
+        content: {
+            type: String,
+            required: true
+        },
+
+        author: {
+            type: String,
+            required: true,
+            trim: true
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = mongoose.model("Blog", blogSchema);
